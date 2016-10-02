801a7b394370cb7a1cc426ec4f308664109c71f9
Sander Vispoel <sandervispoel@hotmail.com>
2016-10-02 12:20:15 +0200

    b53
    IMAGES
    - Notify users when there are no images for a category

--- resources/views/images/category.blade.php
+++ resources/views/images/category.blade.php
@@ -7,13 +7,17 @@
     @include('partials/category_dropdown')
 
     <div class="article-container">
-        @foreach($images as $image)
+        @if($images->count() > 0)
+            @foreach($images as $image)
             <div class="image-thumbnail">
                 <a href="{{ action('ImageController@show', ['image' => $image->image_uri]) }}">
                     <img src="{{ url('uploads/'.$image->image_uri) }}" alt="{{ $image->title }}" title="{{ $image->title }}">
                 </a>
             </div>
-        @endforeach
+            @endforeach
+        @else
+            <span>There are no images in this category.</span>
+        @endif
     </div>
 </section>
 @endsection
