4263eda64e8eb1bab15611ab4607244607a74030
Sander Vispoel <sandervispoel@hotmail.com>
2016-10-30 14:52:01 +0100

    b103
    IMAGES
    - Categories page is now responsive

--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -9,29 +9,30 @@
     <div class="article-container">
         <div class="row">
             @foreach($categories as $category)
-                <div class="category clearfix">
-                    <div class="col-md-2">
-                        <h3>
-                            <a href="{{ url('/images/category/'.$category->name) }}">
-                                {{ ucfirst(trans($category->name)) }}
-                            </a>
-                        </h3>
-                    </div>
-
-                    <div class="col-md-10">
-                    @for ($i = 0; $i < count($images); $i++)
-                        @for ($j = 0; $j < count($images[$i]); $j++)
-                            @if(isset($images[$i][$j]) && $images[$i][$j]['category_id'] == $category->id)
-                            <div class="image-thumbnail-small">
-                                <a href="{{ action('ImageController@show', ['image' => $images[$i][$j]['image_uri']]) }}">
-                                    <img src="{{ url('/uploads/'.$images[$i][$j]['image_uri']) }}"  alt="{{ $images[$i][$j]['title'] }}" title="{{ $images[$i][$j]['title'] }}">
-                                </a>
-                            </div>
-                            @endif
-                        @endfor
-                    @endfor
-                    </div>
+            <div class="category">
+                <div class="col-md-2">
+                    <h3>
+                        <a href="{{ url('/images/category/'.$category->name) }}">
+                            {{ ucfirst(trans($category->name)) }}
+                        </a>
+                    </h3>
                 </div>
+
+                <div class="col-md-10">
+                @for ($i = 0; $i < count($images); $i++)
+                    @for ($j = 0; $j < count($images[$i]); $j++)
+                        @if(isset($images[$i][$j]) && $images[$i][$j]['category_id'] == $category->id)
+                        <div class="image-thumbnail-small">
+                            <div class="dummy"></div>
+                            <a href="{{ action('ImageController@show', ['image' => $images[$i][$j]['image_uri']]) }}">
+                                <img src="{{ url('/uploads/'.$images[$i][$j]['image_uri']) }}"  alt="{{ $images[$i][$j]['title'] }}" title="{{ $images[$i][$j]['title'] }}">
+                            </a>
+                        </div>
+                        @endif
+                    @endfor
+                @endfor
+                </div>
+            </div>
             @endforeach
         </div>
     </div>
